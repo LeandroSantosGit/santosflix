adba8cd2b1b17082805c833a558a1a9e669d8afc
Leandro Santos <rockgolmetal21@gmail.com>
2020-08-04 19:02:56 -0300

    Estilo do formulario cadastro categoria e Eslint

--- src/componentes/FormField/index.js
+++ src/componentes/FormField/index.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function FormField({ label, type, name, value, onChange }) {
-    return (
-        <div>
-            <label>
-                {label}: 
-                <input
-                    type={type}
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                />
-            </label>
-        </div>
-    );
+import { FormFieldWrapper, Label, Input } from './style';
+
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+  const isTextArea = type === 'textarea';
+  const tag = isTextArea ? 'textarea' : 'input';
+  const hasValue = Boolean(value.length);
+  
+  return (
+    <FormFieldWrapper>
+      <Label htmlFor={fieldId}>
+        <Input
+          as={tag}
+          id={fieldId}
+          type={type}
+          name={name}
+          value={value}
+          hasValue={hasValue}
+          onChange={onChange}
+        />
+        <Label.Text>
+            {label}
+            :
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
+}
+
+FormField.defaultProps = {
+    type: 'text',
+    value: '',
+    onChange: () => {}
+}
+
+FormField.propTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
 }
 
 export default FormField;
